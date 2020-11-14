9564aba7cf66eabf7c12452ce4f34af0c9035116
yxh <18903651472@163.com>
2020-11-14 15:38:48 +0800

    【标题】HTML模板 【内容】模板方法使前端HTML与后台逻辑分开，同时支持模板继承复用 【是否完成】是 【备注】无

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return 'hello world!'
+	user = {'username': 'Miguel'}	
+	posts = [
+		{
+			'author': {'username':'John'},
+			'body': 'Beautiful day in portland!'
+		},
+		{
+			'author': {'username': 'Susan'},
+			'body': 'The Avengers movie was so coll!'
+		}
+	]
+	return render_template('index.html', title = 'Home', user = user, posts = posts)
